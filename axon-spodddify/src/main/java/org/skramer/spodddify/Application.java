2364fa5ea7ecba3dbd101877a95a030f583edf01
Sebastian Kramer <sebastian.kramer@wp.pl>
2019-04-15 19:24:03 +0200

    fixed running app locally

--- axon-spodddify/src/main/java/org/skramer/spodddify/Application.java
+++ axon-spodddify/src/main/java/org/skramer/spodddify/Application.java
@@ -1,11 +1,22 @@
 package org.skramer.spodddify;
 
+import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
+import org.axonframework.eventsourcing.eventstore.EventStore;
+import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public EventStore embeddedEventStore() {
+        return EmbeddedEventStore.builder()
+                .storageEngine(new InMemoryEventStorageEngine())
+                .build();
     }
 }
