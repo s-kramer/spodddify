2b6bc3d5b62b40b10f9c7ca27858902c13f6dc04
Sebastian Kramer <sebastian.kramer@wp.pl>
2019-05-08 23:07:10 +0200

    adding controllers - WIP

--- axon-spodddify/src/main/java/org/skramer/spodddify/Application.java
+++ axon-spodddify/src/main/java/org/skramer/spodddify/Application.java
@@ -6,8 +6,10 @@ import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageE
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- axon-spodddify/src/main/java/org/skramer/spodddify/payment/BillingAccountController.java
+++ axon-spodddify/src/main/java/org/skramer/spodddify/payment/BillingAccountController.java
@@ -2,11 +2,15 @@ package org.skramer.spodddify.payment;
 
 import java.util.concurrent.CompletableFuture;
 
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
+import org.skramer.spodddify.payment.command.CreateBillingAccountCommand;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import lombok.AllArgsConstructor;
 
@@ -14,9 +18,16 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 class BillingAccountController {
     private final QueryGateway queryGateway;
+    private final CommandGateway commandGateway;
 
     @GetMapping("/billing-account/{billingAccountId}")
     CompletableFuture<BillingAccount> getBillingAccounts(@PathVariable("billingAccountId") String billingAccountId) {
         return queryGateway.query(new GetBillingAccountQuery(billingAccountId), ResponseTypes.instanceOf(BillingAccount.class));
+    }
+
+    @PostMapping("/billing-account")
+    void createBillingAccount(@RequestBody CreateBillingAccountRequest createBillingAccountRequest) {
+        final CompletableFuture<Object> send = commandGateway.send(new CreateBillingAccountCommand(createBillingAccountRequest.getPaymentPlan()));
+        System.out.println(send);
     }
 }
